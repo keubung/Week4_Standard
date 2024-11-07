4e8fb0f4e5e89f97267506567ba32b3abb9812d8
keubung <skdud3390@naver.com>
2024-11-07 19:26:31 +0900

    [Add] Q1 -1, 2
    item_wand 추가

---
+++ Assets/Scripts/Equip/Equip.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Equip : MonoBehaviour
+{
+    public virtual void OnAttackInput()
+    {
+
+    }
+}
